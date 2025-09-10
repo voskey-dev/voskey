884c4d4a28f9ff1497a65f60cea3c1945baffd25
icalo35 <icalo35@gmail.com>
2025-09-11 00:10:10 +0900

    クリップ追加メニュー項目において、表示件数を更に拡張(30→50)

--- packages/frontend/src/cache.ts
+++ packages/frontend/src/cache.ts
@@ -7,7 +7,7 @@ import * as Misskey from 'misskey-js';
 import { Cache } from '@/utility/cache.js';
 import { misskeyApi } from '@/utility/misskey-api.js';
 
-export const clipsCache = new Cache<Misskey.entities.Clip[]>(1000 * 60 * 30, () => misskeyApi('clips/list', { limit: 30 }));
+export const clipsCache = new Cache<Misskey.entities.Clip[]>(1000 * 60 * 30, () => misskeyApi('clips/list', { limit: 50 }));
 export const rolesCache = new Cache(1000 * 60 * 30, () => misskeyApi('admin/roles/list', { limit: 30 }));
 export const userListsCache = new Cache<Misskey.entities.UserList[]>(1000 * 60 * 30, () => misskeyApi('users/lists/list'));
 export const antennasCache = new Cache<Misskey.entities.Antenna[]>(1000 * 60 * 30, () => misskeyApi('antennas/list', { limit: 30 }));
